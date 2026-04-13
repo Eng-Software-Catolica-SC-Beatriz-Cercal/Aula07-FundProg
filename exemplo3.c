0d9bf6e66666f08dc05244a5b8682b2b846c532e
BeatrizCercal <biaccachoeira@gmail.com>
2026-04-13 20:41:12 -0300

    exemplo 3 finalizado

---
+++ exemplo3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    //variáveis
+	int n;
+	/*
+	for(n=1;n<=10;n=n+1){
+		printf("%i. Manfred\n",n);
+	}
+	*/
+	n=1;
+    //processamento e saída
+	while(n<=10){
+		printf("%i. Manfred\n",n);
+		n++;
+	}
+	
+	return 0;
+}
