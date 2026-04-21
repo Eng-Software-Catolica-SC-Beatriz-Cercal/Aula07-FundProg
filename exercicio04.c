7bdadabd2d7eeb485d3f711ef7572deaaffac4e3
BeatrizCercal <biaccachoeira@gmail.com>
2026-04-21 14:43:04 -0300

    exercicio 04 finalizado

---
+++ exercicio04.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    // variáveis
+    int i, quantidade_notas;
+    float media = 0, nota;
+
+    // entrada e processamento
+    printf("Digite a quantidade de notas: ");
+        scanf("%i",&quantidade_notas);
+        fflush(stdin);
+
+    for(int i=0;i<quantidade_notas;i++){
+        printf("Digite a nota: ");
+        scanf("%f",&nota);
+        fflush(stdin);
+
+        media += nota;
+    }
+    
+    media /= quantidade_notas;
+    
+        // saida de dados
+        printf("Media: %.2f\n", media);
+        return 0;
+    }
